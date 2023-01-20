087914431921d46485fb747575b084cb66ce8902
Matheus Balbi <balbioficial@gmil.com>
2023-01-20 15:13:09 -0300

    incremento

---
+++ tipos-variaveis/src/Incremento.java
@@ -0,0 +1,30 @@
+public class Incremento {
+    public static void main(String[] args) {
+       
+        int numero = 5;
+
+        numero = numero + 2;
+
+        //x repeticao
+        numero ++;
+        //numero = numero +1;
+        // mesma regra funcinona para diminuir em decremento com --;
+
+        System.out.println(numero ++);
+
+        System.out.println(numero);
+
+        System.out.println(++ numero);
+
+
+        Boolean variavel = true;
+
+        System.out.println(!variavel);
+
+        //"!"nega a variável boolean
+
+
+    }
+
+    }
+
